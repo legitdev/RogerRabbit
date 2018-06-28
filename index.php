55389d9403bd3a96f330499585c5a261a95a037d
Roger I Reyes <legit.dev@gmail.com>
2018-06-28 14:01:36 -0400

    Small changes to line 8 - 12

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
 <body>
     
 <h1>Roger Rabbit</h1>
-  <?php
-print "<h2>RR Test!</h2>";
-print "Sa Pase!<br>";
-print "Roger Rabbit Test!";
-?>    
+
+    <?php
+        print "<h2>RR Test!</h2>";
+        print "Sa Pase!<br>";
+        print "Roger Rabbit Test!";
+    ?>    
     
 </body>
 </html>
